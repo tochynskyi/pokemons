f3905bc3c2d7cbf86bdd23025a52476f2eff5c83
Serhii Tochynskyi <tochynskyi.s@gmail.com>
2023-02-22 14:48:55 +0200

    fix bug pagination

--- src/components/Pagination/AppPagination.js
+++ src/components/Pagination/AppPagination.js
@@ -5,7 +5,7 @@ import { usePagination } from "../../hooks/usePagination";
 const PER_PAGE = 3;
 
 export const AppPagination = ({ pokemons, setPokemonsList }) => {
-  let [page, setPage] = useState(1);
+  const [page, setPage] = useState(1);
 
   const count = Math.ceil(pokemons.length / PER_PAGE);
   const _DATA = usePagination(pokemons, PER_PAGE);
@@ -16,8 +16,13 @@ export const AppPagination = ({ pokemons, setPokemonsList }) => {
   };
   useEffect(() => {
 	setPokemonsList(_DATA.currentData());
- }, [page, pokemons]);
+ }, [page]);
 
+ useEffect(() => {
+	setPage(1)
+	_DATA.jump(1);
+	setPokemonsList(_DATA.currentData());
+ }, [pokemons]);
 
   return (
     <Box>
